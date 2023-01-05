dadd43d16e479ef4bdbb13da8c6f5b7b28b14d4f
FATIHA99 <fatihhaa27@gmail.com>
2023-01-05 11:23:05 +0100

    réalisation de la partie register pour Employee

--- back-end/Controllers/Employee-Controller.js
+++ back-end/Controllers/Employee-Controller.js
@@ -1,13 +1,22 @@
 const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
+
 function AjouterEmpolyee(req, res) {
     const { body } = req
-     const hashPassword =  bcrypt.hashSync(body.pasword)
-    User.create({ ...body })
-
-        .then((e) => { res.json('EMPLOYEE ADDED') })
-        .catch()
+    const hashPassword = bcrypt.hashSync(body.password, 10)
+    body.password = hashPassword
+    User.findOne({ email: body.email })
+        .then((e) => {
+            if (e) {
+                res.json({ response: 'email already exist' })
+            }
+            else {
+                User.create({ ...body })
+                    .then((e) => { res.json({ response: 'EMPLOYEE ADDED' }) })
+                    .catch()
+            }
+        })
 }
 
 module.exports = { AjouterEmpolyee }
