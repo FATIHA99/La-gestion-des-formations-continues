e270e42ce120ca90651ea40935036c72c5f7602b
FATIHA99 <fatihhaa27@gmail.com>
2023-01-08 02:17:12 +0100

    consomation API Update organisme

--- front-end/src/Components/Organisme/Modal-Update-Organisme.jsx
+++ front-end/src/Components/Organisme/Modal-Update-Organisme.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
@@ -14,71 +14,72 @@ function Example(props) {
     const handleShow = () => setShow(true);
 
     const [oneOrganisme, setOrganisme] = useState({ label: '', compus: '' });
-
+ 
 
     const getOne = async (e, id) => {
         e.preventDefault()
-
         const One = await axios.get(`${END_POINT}/organisme/getone/${id}`)
         setOrganisme((One.data));
         console.log(One.data)
-
         handleShow()
-
     }
+
+    const updateOrganisme = (e, id) => {
+        axios.put(`${END_POINT}/organisme/update/${id}`,oneOrganisme)
+            .then((e) => {
+                toast.success('organisme updated ')
+                handleClose();
+            })
+    }
+ 
 
 
     return (
 
         <>
-
-
             <button onClick={(e) => { getOne(e, props.id) }} className="btn  btn-outline-info"  ><i className="bi bi-pencil-square"></i> </button>
 
             <Modal show={show} onHide={handleClose} >
                 <Modal.Header className='bg-dark text-light' closeButton>
                     <Modal.Title>Add organisme</Modal.Title>
                 </Modal.Header>
-               
+                <Modal.Body className='bg-dark'>
+                    <Form >
+                        <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
+                            <Form.Label className='text-light'>Label</Form.Label>
+                            <Form.Control
+                                onChange={(e) => { setOrganisme({ ...oneOrganisme, [e.target.name]: e.target.value }) }}
+                                type="text"
+                                value={oneOrganisme.label}
+                                name='label'
+                                placeholder="enter the label of organisme"
+                                autoFocus
+                            />
+                        </Form.Group>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                        >
+                            <Form.Label className='text-light'>Compus</Form.Label>
+                            <Form.Control
+                                onChange={(e) => { setOrganisme({ ...oneOrganisme, [e.target.name]: e.target.value }) }}
+                                type="text"
+                                value={oneOrganisme.compus}
+                                name="compus"
+                                placeholder="enter the compus of organisme"
+                                autoFocus
 
+                            />
 
-                    <Modal.Body className='bg-dark'>
-                        <Form >
-                            <Form.Group className="mb-3 " controlId="exampleForm.ControlInput1">
-                                <Form.Label className='text-light'>Label</Form.Label>
-                                <Form.Control
-
-                                    type="text"
-                                    value={oneOrganisme.label}
-                                    name='label'
-                                    placeholder="enter the label of organisme"
-                                    autoFocus
-                                />
-                            </Form.Group>
-                            <Form.Group
-                                className="mb-3"
-                                controlId="exampleForm.ControlTextarea1"
-                            >
-                                <Form.Label className='text-light'>Compus</Form.Label>
-                                <Form.Control
-
-                                    type="text"
-                                    value={oneOrganisme.compus}
-                                    name="compus"
-                                    placeholder="enter the compus of organisme"
-                                    autoFocus
-
-                                />
-
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer className='bg-dark'>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer className='bg-dark'>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="info" onClick={handleClose}>
-                        Add Organisme
+                    <Button variant="info" onClick={(e) => { updateOrganisme(e, props.id) }} >
+                        update
                     </Button>
                 </Modal.Footer>
             </Modal>
